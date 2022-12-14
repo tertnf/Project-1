c5ba80c403f9d3ca5fc88ce15f798824d0d66f0c
Lida Deineko <93deineko@gmail.com>
2022-12-14 14:18:56 +0200

    Add slick-slider settings

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//slider-1
+
+$(".qualification-list_slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true
+  });
+  
