4aca3f878f75bc48f792bf27adb3003229c29aec
Nataliia Tertyshna <tertnf@gmail.com>
2023-01-28 19:54:11 +0200

    mobile-menu
    mobile-menu

--- js/appointment-form.js
+++ js/appointment-form.js
@@ -1,19 +1,19 @@
-// const form = document.getElementById("form");
-// console.log(form);
+const form = document.getElementById("form");
+console.log(form);
 console.log("hello word");
-// function retrieveFormValue(event) {
-//   // event.preventDefult();
-//   // const name = form.querySelector('[name="name"]'),
-//   //   tel = form.querySelector('[name="tel"]');
+function retrieveFormValue(event) {
+  event.preventDefult();
+  const name = form.querySelector('[name="name"]');
+  const tel = form.querySelector('[name="tel"]');
 
-//   // const values = {
-//   //   name: name.value,
-//   //   tel: tel.value,
-//   // };
+  const values = {
+    name: name.value,
+    tel: tel.value,
+  };
 
-//   // console.log(values);
-//   let message =
-//     "Дякую, ваші данні успішно записані, я зв'яжуся з вами дуже скоро, Наталія Угай.";
-//   alert(message);
-// }
-// form.addEventListener("submit", retrieveFormValue);
+  console.log(values);
+  let message =
+    "Дякую, ваші данні успішно записані, я зв'яжуся з вами дуже скоро, Наталія Угай.";
+  alert(message);
+}
+form.addEventListener("submit", retrieveFormValue);
