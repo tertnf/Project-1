32b35af5a1700a203d7b38fb3e37b1ccce686bc8
Nataliia Tertyshna <tertnf@gmail.com>
2023-02-10 18:05:54 +0200

    feedback slider desktop bug fix
    feedback slider desktop bug fix

--- js/feedbackSlider.js
+++ js/feedbackSlider.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
-  $(".feedbacks").slick();
+  $(".feedbacks").slick({
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    easing: "linear",
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // {
+      //   breakpoint: 650,
+      //   settings: {
+      //     slidesToShow: 1,
+      //     slidesToScroll: 1,
+      //   },
+      // },
+    ],
+  });
 });
-$(".feedbacks").slick({
-  infinite: true,
-  speed: 500,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-});
+
 // $(".slider").slick("setPosition");
 // $(".data").slick();
 // $(".feedback-arrow-left").click(function (event) {
